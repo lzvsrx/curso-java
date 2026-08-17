57a4657eda2eceeb68dca74851f10c42a2a3883e
lzvsrx <valenzisousaluizotavio@outlook.com>
2026-08-17 10:54:50 -0300

    Add variable types exercises

---
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -0,0 +1,16 @@
+public class TiposVariaveis {
+    public static void main(String[] args) throws Exception {
+        double salarioMinimo = 2500;
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short) numeroNormal;
+        System.out.println(salarioMinimo);
+        System.out.println(numeroCurto);
+        System.out.println(numeroNormal);
+        System.out.println(numeroCurto2);
+        int numero = 1;
+        numero = 10;
+        System.out.println(numero);
+        final double Valor_De_Pi = 3.14;
+    }
+}
