8abdc08ddc02df61d18f956bf4126504d50ed888
lzvsrx <valenzisousaluizotavio@outlook.com>
2026-08-17 10:40:00 -0300

    Delete java-anatomia-classes directory

--- java-anatomia-classes/src/edu/luizotavio/segundasemana/MinhaClasse.java
+++
@@ -1,6 +0,0 @@
-package edu.luizotavio.segundasemana;
-public class MinhaClasse {
-    public static void main1(String[] args){
-        System.out.print("Ola sejam bem vindos ao curso de Java");
-    }
-}
--- java-anatomia-classes/src/edu/luizotavio/segundasemana/MinhaClasses.java
+++
@@ -1,9 +0,0 @@
-package edu.luizotavio.segundasemana;
-public class MinhaClasses {
-    static final int ANO = 2026;
-    public static void main(String[] args){
-        String BR = "Brasil";
-        System.out.println("O país é: " + BR);
-        System.out.println("O ano é: " + ANO);
-    }
-}
