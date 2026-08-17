644dcc92ff418f737ddfe974955112fce632a426
lzvsrx <valenzisousaluizotavio@outlook.com>
2026-08-17 11:22:04 -0300

    Add conditional operator examples

--- tipos-variaveis/src/Operadores.java
+++ tipos-variaveis/src/Operadores.java
@@ -28,5 +28,17 @@ public class Operadores {
         System.out.println(numero --);
         boolean variavel2 = true;
         System.out.println(!variavel2);
+        int a, b;
+        a = 5;
+        b = 6;
+        String resultado = "";
+        if(a == b)
+            resultado = "verdadeiro";
+        else
+            resultado = "falso";
+
+        System.out.println(resultado);
+        String resultado2 = a != b ? "verdadeiro" : "falso";
+        System.out.println(resultado2);
     }
 }
