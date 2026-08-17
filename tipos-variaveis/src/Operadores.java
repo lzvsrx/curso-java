7e2a16de3369bbacd622b9c32e7f51c2c00e7140
lzvsrx <valenzisousaluizotavio@outlook.com>
2026-08-17 11:24:33 -0300

    Add relational operator examples

--- tipos-variaveis/src/Operadores.java
+++ tipos-variaveis/src/Operadores.java
@@ -40,5 +40,17 @@ public class Operadores {
         System.out.println(resultado);
         String resultado2 = a != b ? "verdadeiro" : "falso";
         System.out.println(resultado2);
+        int numero1 = 1;
+        int numero2 = 2;
+        boolean simNao = numero1 > numero2;
+        boolean simNao2 = numero1 < numero2;
+        boolean simNao3 = numero1 >= numero2;
+        boolean simNao4 = numero1 <= numero2;
+        boolean simNao5 = numero1 == numero2;
+        System.out.println("numero1 > numero2 " + simNao);
+        System.out.println("numero1 < numero2 " + simNao2);
+        System.out.println("numero1 >= numero2 " + simNao3);
+        System.out.println("numero1 <= numero2 " + simNao4);
+        System.out.println("numero1 == numero2 " + simNao5);
     }
 }
