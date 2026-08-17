516c724a74161d9a13d14831c4e9cd70fbbcbca1
lzvsrx <valenzisousaluizotavio@outlook.com>
2026-08-17 11:27:30 -0300

    Add comparison condition examples

--- tipos-variaveis/src/Operadores.java
+++ tipos-variaveis/src/Operadores.java
@@ -47,6 +47,26 @@ public class Operadores {
         boolean simNao3 = numero1 >= numero2;
         boolean simNao4 = numero1 <= numero2;
         boolean simNao5 = numero1 == numero2;
+        if(numero1 == numero2)
+            System.out.println("a nossa condição é verdadeira");
+        else
+            System.out.println("a nossa condição é falsa");
+        if(numero1 > numero2)
+            System.out.println("a nossa condição é verdadeira");
+        else
+            System.out.println("a nossa condição é falsa");
+        if(numero1 < numero2)
+            System.out.println("a nossa condição é verdadeira");
+        else
+            System.out.println("a nossa condição é falsa");
+        if(numero1 >= numero2)
+            System.out.println("a nossa condição é verdadeira");
+        else
+            System.out.println("a nossa condição é falsa");
+        if(numero1 <= numero2)
+            System.out.println("a nossa condição é verdadeira");
+        else
+            System.out.println("a nossa condição é falsa");
         System.out.println("numero1 > numero2 " + simNao);
         System.out.println("numero1 < numero2 " + simNao2);
         System.out.println("numero1 >= numero2 " + simNao3);
